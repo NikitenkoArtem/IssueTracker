0bc1196ed1a24a9f1cc45942096b71e21c3d981a
git <ghost-hero2012@mail.ru>
2016-09-14 00:45:02 +0300

    So much refactor in Issue class

--- src/by/epam/Project.java
+++ src/by/epam/Project.java
@@ -6,62 +6,69 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by Price on 07.09.2016.
  */
 @WebServlet(name = "Project")
 public class Project extends HttpServlet {
-    private final String newProject = "insert into projects values(?, ?, ?, ?)";
-    private final String changeProject = "update projects set project_name=?, description=?, build=?, manager=?";
-    private final String projectList = "";
+    private final String insertProject = "INSERT INTO projects VALUES(?, ?, ?, ?)";
+    private final String updateProject = "UPDATE projects SET project_name=?, description=?, build=?, manager=?";
+    private final String selectProject = "SELECT project_name, manager_name, description FROM projects " +
+            "INNER JOIN managers m ON manager = m.manager_id";
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        final String name = request.getParameter("name");
+        final String desc = request.getParameter("desc");
+        final String build = request.getParameter("build");
+        final String manager = request.getParameter("manager");
+        HashMap<Integer, String> table = new HashMap<>();
+        table.put(1, name);
+        table.put(2, desc);
+        table.put(3, build);
+        table.put(4, manager);
+        new DBConnection().execUpdate(insertProject, table);
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        try (Connection conn = DBConnection.getConnection()) {
-//            request.setAttribute("projects", getInfo(conn, projectList));
-            request.getRequestDispatcher("/admin/projects/project.jsp").forward(request, response);
-        } catch (SQLException e) {
-            e.printStackTrace();
+        final String action = request.getParameter("action");
+        if (action.equals("list")) {
+            try (Connection conn = DBConnection.getConnection()) {
+                request.setAttribute("projects", projects(conn));
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
+        request.getRequestDispatcher("/admin/projects/project.jsp").forward(request, response);
     }
 
-    private void addProject(HttpServletRequest request) throws SQLException {
-        try (Connection conn = DBConnection.getConnection()) {
-            try (PreparedStatement stmt = conn.prepareStatement(newProject)) {
-                stmt.setString(1, request.getParameter("proj_name"));
-                stmt.setString(2, request.getParameter("desc"));
-                stmt.setString(3, request.getParameter("build"));
-                stmt.setString(4, request.getParameter("manager"));
-                stmt.executeUpdate();
+    private StringBuffer projects(Connection conn) throws SQLException, IOException {
+        StringBuffer buffer = null;
+        String openTr = "<tr>";
+        String closeTr = "</tr>";
+        String openTd = "<td>";
+        String closeTd = "</td>";
+        try (PreparedStatement stmt = conn.prepareStatement(selectProject)) {
+            try (ResultSet rs = stmt.executeQuery()) {
+                buffer = new StringBuffer();
+                while (rs.next()) {
+                    final String projectName = rs.getString("project_name");
+                    final String managerName = rs.getString("manager_name");
+                    final String description = rs.getString("description");
+                    buffer.append(openTr);
+                    buffer.append(openTd + "<a href='/project?projectName=" + projectName + "'>" + projectName + "</a>" + closeTd);
+                    buffer.append(openTd + managerName + closeTd);
+                    buffer.append(openTd + description + closeTd);
+                    buffer.append(closeTr);
+                }
             }
-        } catch (SQLException e) {
-            e.printStackTrace();
         }
-    }
-
-    private void editProject(HttpServletRequest request) throws SQLException {
-        try (Connection conn = DBConnection.getConnection()) {
-            try (PreparedStatement stmt = conn.prepareStatement(changeProject)) {
-//                stmt.setString(1, request.getParameter("proj_name"));
-//                stmt.setString(2, request.getParameter("desc"));
-//                stmt.setString(3, request.getParameter("build"));
-//                stmt.setString(4, request.getParameter("manager"));
-//                stmt.executeUpdate();
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        return buffer;
     }
 }
